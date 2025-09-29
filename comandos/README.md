0c832ec5d071032d0713a51ffac497bd2891ca8e
Dynnho <dynnho23@hotmail.com>
2025-09-29 20:49:21 -0300

    Adiciona funcionalidades ao app de contatos: implementa modelos, views e templates; atualiza configurações e arquivos de migração.

--- comandos/README.md
+++ comandos/README.md
@@ -1,7 +1,7 @@
 python -m venv venv
 . venv/bin/activate (MAC/Linux)
 .\venv\Scripts\activate (Windows)
-pip install django
+python -m pip install django
 django-admin startproject project .
 python manage.py startapp contact
 
@@ -19,13 +19,37 @@ git remote add origin URL_DO_GIT
 
 # Migrando a base de dados do Django
 
-python manage.py makeemigrations
+python manage.py makemigrations
 python manage.py migrate
 
 # Criando e modificando a senha de um super usuário Django
 
 python manage.py createsuperuser
 python manage.py changepassword USERNAME
+
+# Trabalhando com o model do Django
+
+# Importa o módulo
+from contact.models import Contact
+# Cria um contato (Lazy)
+# Retorna o contato
+contact = Contact(**fields)
+contact.save()
+# Cria um contato (Não lazy)
+# Retorna o contato
+contact = Contact.objects.get(pk=10)
+# Edita um contato
+# Retorna o contato
+Contact.field_name1 = 'Novo valor 1'
+contact.field_name2 = 'Novo valor 2'
+contact.save()
+# Apaga um contato
+# Depende da base de dados, geralmente retorna o número
+# de valores manipulados na base de dados
+contacts = Contact.objects.all().order_by('id')
+# Seleciona cintatos usandi filtros
+# Retorna QuerySet[]
+contacts = Contact.objets.filter(**filters).order_by('-id')
 
 # Fim (TESTE)
 #
--- contact/models.py
+++ contact/models.py
@@ -1,12 +1,27 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # id (primary key - automático)
 # First_name (string), last_name (string), phone (string),
 # email (email), created_date (data), description (text)
-# category (foreign key), show (boolean), owner (foreign key)
-# picture (imagem)
+# category (foreign key), show (boolean), picture (imagem)
 
+# Depois
+# owner (foreign key)
+
+
+class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+       return self.name
+   
+   
 class Contact(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -14,6 +29,12 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='picutures/%Y/%m/%d')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True,
+                                 blank=True)
 
+    owner = models.ForeignKey(User, on_delete=models.SET_NULL, null=True,
+                                 blank=True)
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
